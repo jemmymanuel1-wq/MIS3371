53eb8b4154f82f0a3eb409140c17d278331eeca3
jemmymanuel1-wq <jemmymanuel1@gmail.com>
2026-06-25 12:20:57 -0500

    Implement reviewForm to display user input
    Added reviewForm function to gather and display user data.

--- homework2/script.js
+++ homework2/script.js
@@ -7,3 +7,21 @@ window.onload = function () {
         "Today is: " + today.toDateString();
 
 };
+
+function reviewForm() {
+
+    // STEP 1: Get values from form inputs (REAL USER DATA ONLY)
+    const first = document.getElementById("firstname").value;
+    const last = document.getElementById("lastname").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+
+    // STEP 2: Display them inside reviewOutput div
+    document.getElementById("reviewOutput").innerHTML =
+        "<h3>PLEASE REVIEW THE INFORMATION</h3>" +
+        "<p><strong>First Name:</strong> " + first + "</p>" +
+        "<p><strong>Last Name:</strong> " + last + "</p>" +
+        "<p><strong>Email:</strong> " + email + "</p>" +
+        "<p><strong>Phone:</strong> " + phone + "</p>";
+
+}
