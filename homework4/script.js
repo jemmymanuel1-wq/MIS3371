02adcf8960e660c03466dff3460f85d40e628bd5
jemmymanuel1-wq <jemmymanuel1@gmail.com>
2026-07-11 13:01:03 -0500

    Fix typo in updateHealthValue function call

--- homework4/script.js
+++ homework4/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("date").innerHTML =
         "Today is: " + today.toDateString();
     
-    updatedHealthValue();
+    updateHealthValue();
     loadStates();
 });
 
